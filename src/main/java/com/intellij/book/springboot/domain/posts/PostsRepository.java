46fb2a7ffb05989535dc26d2ea5597da3b491b47
nonemanner <52825803+nonemanner@users.noreply.github.com>
2021-02-09 21:23:02 +0900

    Spring Data JPA JPA Auditing 생성시간/수정시간

---
+++ src/main/java/com/intellij/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.intellij.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
